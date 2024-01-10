e1afc6e3d37816953416cfb4fb03af76372fe05a
Michael Altobelli <maltob@users.noreply.github.com>
2024-01-10 02:49:45 +0000

    Added support for GZip compressed images based on content-type header

--- examples/wasi-browser/htdocs/worker.js
+++ examples/wasi-browser/htdocs/worker.js
@@ -15,6 +15,47 @@ onmessage = (msg) => {
     var netParam = getNetParam();
     var listenfd = 3;
     fetch(getImagename(), { credentials: 'same-origin' }).then((resp) => {
+        const contentType = resp.headers.get('content-type');
+
+        if(contentType == null) {
+            throw Error('Failed to detect image encoding');
+        }
+
+
+        //Stream the response through the GZip decoder if needed. Supports browsers since about 2023.
+        return new Response(
+            new ReadableStream({
+
+                start(respController) {
+                    var respBody = resp.body;
+
+                    if(contentType == "application/x-gzip") {
+                        respBody = respBody.pipeThrough( new DecompressionStream("gzip"));
+                     }
+
+                     const respReader = respBody.getReader();
+                     readResponse(self);
+
+                    function readResponse(self) {
+                        respReader.read(self).then(({done,value}) => {
+                            if(done) {
+                                respController.close()
+                                return;
+                            }
+
+                            respController.enqueue(value);
+                            readResponse(self);
+                        }).catch(error => {
+                            console.error(error);
+                            respController.error(error);
+                        })
+                    }
+
+                }
+            })
+        );
+
+    }).then((resp) => {
         resp['arrayBuffer']().then((wasm) => {
             if (netParam) {
                 if (netParam.mode == 'delegate') {
